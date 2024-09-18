526e43a7caddcf63cee2766f50350b4bb11c49f1
Sohel0188 <sohelsikder.cse@gmail.com>
2024-09-18 23:08:53 +0600

    sum of two number

---
+++ sum_of_two_value_using_function.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int sumoftwoNumber(int a,int b){
+    int c = a+b;
+    return c;
+}
+int main() {
+   int num1,num2;
+   cin>>num1>>num2;
+
+  int sum = sumoftwoNumber(num1,num2);
+   cout<<"the sum is = "<<sum;
+    return 0;
+}
