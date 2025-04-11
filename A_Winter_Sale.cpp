5a57cee269207b92cc8d66126aca977c58a0c340
Sohel0188 <sohelsikder.cse@gmail.com>
2025-04-11 23:57:23 +0600

    winter sale

--- A_Winter_Sale.cpp
+++ A_Winter_Sale.cpp
@@ -1,7 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    float x,p;
+    cin>>x>>p;
+    float result = (p*100)/(100-x);
+    cout <<fixed<<setprecision(2) <<result<<endl;
     return 0;
 }
 
